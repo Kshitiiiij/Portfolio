84a8b40b31b189d1dbdb9fa5b2459e6f660c3198
kshitij <kshitij_987@protonmail.com>
2023-03-17 13:35:25 +0530

    added home component on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './component/header/Header'
+import Home from './component/home/Home'
 
 function App() {
 
   return (
     <>
     <Header />
+    <Home />
     </>
   )
 }
