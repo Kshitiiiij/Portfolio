0cc0bb051936fb095648a4244d6ddbefa1428be8
kshitij <kshitij_987@protonmail.com>
2023-03-17 23:06:39 +0530

    Added About component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './component/header/Header'
 import Home from './component/home/Home'
+import About from './component/about/About'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   )
