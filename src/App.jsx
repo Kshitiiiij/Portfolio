a5bb56c2ff55bc715009b8fe68628928bd70b2fb
kshitij <kshitij_987@protonmail.com>
2023-03-17 13:08:18 +0530

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Header from './component/header/Header'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+    <Header />
+    </>
   )
 }
 
