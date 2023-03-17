9db10860701f1ec5b56853afa2a1e27d79636f2d
kshitij <kshitij_987@protonmail.com>
2023-03-17 13:52:18 +0530

    corrected class name social icons

--- src/component/home/Social.jsx
+++ src/component/home/Social.jsx
@@ -2,11 +2,11 @@ export default function Social() {
     return (
         <div className="home__social">
             <a href="https://www.instagram.com/kshitij_k2" className="home__social " target="_blank">
-                <i className="uli uli-instagram"></i>
+            <i class="uil uil-instagram"></i>
             </a>
 
             <a href="https://github.io/" className="home__social " target="_blank">
-                <i className="uli uli-github-alt"></i>
+            <i class="uil uil-github-alt"></i>
             </a>
 
             <a href="https://in.linkedin.com/" className="home__social " target="_blank">
