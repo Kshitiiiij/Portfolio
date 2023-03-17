084b5a3aaf4ce7c46ee4dbf3fb7596d286edcb56
kshitij <kshitij_987@protonmail.com>
2023-03-17 13:46:52 +0530

    added social icons

---
+++ src/component/home/Social.jsx
@@ -0,0 +1,17 @@
+export default function Social() {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/kshitij_k2" className="home__social " target="_blank">
+                <i className="uli uli-instagram"></i>
+            </a>
+
+            <a href="https://github.io/" className="home__social " target="_blank">
+                <i className="uli uli-github-alt"></i>
+            </a>
+
+            <a href="https://in.linkedin.com/" className="home__social " target="_blank">
+            <i class="uil uil-linkedin"></i>
+            </a>
+        </div>
+    )
+}
