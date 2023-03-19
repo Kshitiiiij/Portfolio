d2afb374293876ddb8c8e02d53b4ced04b666851
kshitij <kshitij_987@protonmail.com>
2023-03-19 18:54:55 +0530

    some changed

---
+++ src/component/portfolio/Portfolio.jsx
@@ -0,0 +1,11 @@
+import './Portfolio.css'
+const Portfolio = () => {
+  return (
+    <section className="work section" id="portfolio">
+        <h2 className="section__title">Portfolio</h2>
+        <div className="section__subtitle">Projects</div>
+    </section>
+  )
+}
+
+export default Portfolio
